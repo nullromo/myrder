e32f86349eae44283d995626983df63faff33d47
Kyle Kovacs <kylerobertkovacs@gmail.com>
2026-03-18 18:54:27 -0700

    update readme

--- README.md
+++ README.md
@@ -2,14 +2,3 @@
 
 Website for a shared-deck Magic the Gathering format. Check out
 [myrder.com](myrder.com).
-
-## TODO
-
-- headline and business card
-- pages
-    - rules
-    - changelog
-- contact info
-- link to github
-- link to $ support page
-- non-affiliation disclaimer
--- src/tabs/aboutTab.tsx
+++ src/tabs/aboutTab.tsx
@@ -2,7 +2,7 @@ export const AboutTab = () => {
     return (
         <>
             {
-                'About information goes here. It should have links to the gihub page, the support ($) page, the business card, and the disclaimer information.'
+                'About information goes here. It should have links to the gihub page, the support ($) page, the business card, my contact info, and the disclaimer information.'
             }
         </>
     );
