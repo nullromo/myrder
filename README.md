ec1cf228a371eda3e06b366fa23ec8d26cd43da3
Kyle Kovacs <kylerobertkovacs@gmail.com>
2026-03-25 19:16:39 -0700

    add scroll to top button

--- README.md
+++ README.md
@@ -2,10 +2,3 @@
 
 Website for a shared-deck Magic the Gathering format. Check out
 [myrder.com](https://myrder.com).
-
-## TODO
-
-- preload images
-- make website work with www.myrder.com as well as myrder.com
-- consider adding scroll to top button
-- consider using router to map pages
--- src/layout/body.tsx
+++ src/layout/body.tsx
@@ -1,8 +1,24 @@
+import * as React from 'react';
+import { myrderBlue } from '../util/colors';
 import { BodyTabs } from './bodyTabs';
 import { PictureTitle } from './pictureTitle';
 import { Tagline } from './tagline';
 
 export const Body = () => {
+    const [showScrollToTop, setShowScrollToTop] = React.useState(false);
+
+    React.useEffect(() => {
+        window.onscroll = () => {
+            setShowScrollToTop(
+                document.body.scrollTop > 0 ||
+                    document.documentElement.scrollTop > 0,
+            );
+        };
+        return () => {
+            window.onscroll = null;
+        };
+    }, []);
+
     return (
         <div style={{ display: 'flex', flexDirection: 'column', flexGrow: 1 }}>
             <PictureTitle />
@@ -19,6 +35,32 @@ export const Body = () => {
             >
                 <Tagline />
                 <BodyTabs />
+                {showScrollToTop ? (
+                    <a
+                        href='#'
+                        style={{
+                            background: myrderBlue,
+                            borderRadius: '10px',
+                            bottom: 0,
+                            boxShadow: '0px 0px 2px 2px black',
+                            color: 'inherit',
+                            cursor: 'pointer',
+                            fontFamily: 'MPlantin',
+                            margin: '0 20px 12px 0',
+                            padding: '10px',
+                            position: 'fixed',
+                            right: 0,
+                            textDecoration: 'none',
+                            touchAction: 'manipulation',
+                            userSelect: 'none',
+                        }}
+                        onClick={() => {
+                            console.log();
+                        }}
+                    >
+                        Scroll to top
+                    </a>
+                ) : null}
             </div>
         </div>
     );
